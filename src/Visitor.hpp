89be884fe92202fe3db8995544d4153bc376510c
unknown <miika.lehtimaki@gmail.com>
2016-12-28 02:39:12 +0200

    Add comments in Visitor.hpp

--- src/Visitor.hpp
+++ src/Visitor.hpp
@@ -5,9 +5,15 @@
 namespace fug {
 
 
+    //  Visitor class provides a CRTP interface for derived
+    //  visitors for arbitrary component combinations
     template <typename T_Visitor, typename... T_Components>
     class Visitor {
     public:
+        //  Functor operator taking components as a reference
+        //  is implemented in derived classes.
+        //  Components are being provided by cene when
+        //  the visitor is accepted.
         void operator()(T_Components&... components);
     };
 
