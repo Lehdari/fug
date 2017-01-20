118a5ec17f85f1bdcf7b17e52e6ff6fe4230c4ae
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-01-20 23:45:50 +0200

    add binary

---
+++ include/Core/Binary.hpp
@@ -0,0 +1,23 @@
+#ifndef FUG_BINARY_HPP
+#define FUG_BINARY_HPP
+
+#include "Core/Resource.hpp"
+
+#include <cstddef>
+
+namespace fug {
+    class FUG_RESOURCE(Binary) {
+    public:
+        FUG_RESOURCE_INIT_DESTROY_DECL
+        Binary(void);
+        Binary(const Binary& other);
+        ~Binary(void);
+        const char *getBufferPtr(void) const;
+        size_t getBufferSize(void) const;
+    private:
+        char* buffer_;
+        size_t size_;
+    };
+}
+
+#endif // FUG_BINARY_HPP
---
+++ src/Core/Binary.cpp
@@ -0,0 +1,31 @@
+#include "Core/Binary.hpp"
+
+#include <cstring>
+
+using namespace fug;
+
+Binary::Binary(void) :
+    buffer_(nullptr),
+    size_(0)
+{
+}
+
+Binary::Binary(const Binary& other)
+{
+    buffer_ = new char[other.size_];
+    memcpy(buffer_, other.buffer_, other.size_);
+    size_ = other.size_;
+}
+
+const char *Binary::getBufferPtr(void) const {
+    return buffer_;
+}
+
+size_t Binary::getBufferSize(void) const {
+    return size_;
+}
+
+Binary::~Binary(void) {
+    if (buffer_)
+        delete[] buffer_;
+}
