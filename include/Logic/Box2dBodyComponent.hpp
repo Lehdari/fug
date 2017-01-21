69fec33e7a2b20771de817d46a946cd7cfea78e2
Veikka Kähkönen <insiding@gmail.com>
2017-01-21 06:13:07 +0200

    Add move constructor and assignment operator to Box2dBodyComponent
    Also, add the Logic source directory to CMakeLists.

--- include/Logic/Box2dBodyComponent.hpp
+++ include/Logic/Box2dBodyComponent.hpp
@@ -10,8 +10,14 @@ namespace fug {
 
     class Box2dBodyComponent : public Component {
     public:
+        Box2dBodyComponent();
         Box2dBodyComponent(b2Body* body);
+        Box2dBodyComponent(Box2dBodyComponent&& other);
+        Box2dBodyComponent& operator=(Box2dBodyComponent&& other);
         ~Box2dBodyComponent();
+
+        Box2dBodyComponent& operator=(const Box2dBodyComponent& other) = delete;
+        Box2dBodyComponent(const Box2dBodyComponent& other) = delete;
 
         static b2World* world;
         b2Body* body;
--- src/Logic/Box2dBodyComponent.cpp
+++ src/Logic/Box2dBodyComponent.cpp
@@ -6,9 +6,23 @@ using namespace fug;
 
 b2World* Box2dBodyComponent::world = nullptr;
 
+Box2dBodyComponent::Box2dBodyComponent()
+{}
+
 Box2dBodyComponent::Box2dBodyComponent(b2Body* body) :
     body(body)
 {}
+
+Box2dBodyComponent::Box2dBodyComponent(Box2dBodyComponent&& other) {
+    body = other.body;
+    other.body = nullptr;
+}
+
+Box2dBodyComponent& Box2dBodyComponent::operator=(fug::Box2dBodyComponent &&other) {
+    body = other.body;
+    other.body = nullptr;
+    return *this;
+}
 
 Box2dBodyComponent::~Box2dBodyComponent() {
     if(world) {
