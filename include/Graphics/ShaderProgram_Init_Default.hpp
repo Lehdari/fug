0614b342060d57b7dd2d9f7c685a1eab3007ca0b
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-01-21 22:45:38 +0200

    Add ShaderProgram_Init_Default

---
+++ include/Graphics/ShaderProgram_Init_Default.hpp
@@ -0,0 +1,38 @@
+#ifndef SHADER_PROGRAM_INIT_DEFAULT_HPP
+#define SHADER_PROGRAM_INIT_DEFAULT_HPP
+
+#include "Graphics/ShaderProgram.hpp"
+#include "Graphics/ShaderObject.hpp"
+
+namespace fug {
+    struct ShaderProgramInitInfo_Default {
+    };
+    FUG_RESOURCE_INIT(ShaderProgram, ShaderProgramInitInfo_Default)
+    {
+        if (initResources.size() == 0)
+            return;
+        programId_ = glCreateProgram();
+        for (auto& initResourceId : initResources) {
+            const auto &id = FUG_RESOURCE_MANAGER.getResource<ShaderObject>(initResourceId)->getId();
+            glAttachShader(programId_, id);
+        }
+        glLinkProgram(programId_);
+        GLint linkStatus, infoLogLength;
+        glGetProgramiv(programId_, GL_LINK_STATUS, &linkStatus);
+
+        if (linkStatus == GL_FALSE) {
+            glGetShaderiv(programId_, GL_INFO_LOG_LENGTH, &infoLogLength);
+            char *infoLog = new char[infoLogLength];
+            glGetProgramInfoLog(programId_, infoLogLength, NULL, infoLog);
+            fprintf(stderr, "%s", infoLog);
+            throw infoLog;
+        }
+    }
+    FUG_RESOURCE_DESTROY(ShaderProgram, ShaderProgramInitInfo_Default)
+    {
+        if (programId_ != 0)
+            glDeleteProgram(programId_);
+    }
+}
+
+#endif // SHADER_PROGRAM_INIT_DEFAULT_HPP
