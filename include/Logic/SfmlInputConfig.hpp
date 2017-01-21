aedd15ea3ab8ab632bb6b5ae83e9a8a507bc2b04
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-01-21 17:13:17 +0200

    Add a hash function for sf::Keyborad::Key

--- include/Logic/SfmlInputConfig.hpp
+++ include/Logic/SfmlInputConfig.hpp
@@ -11,13 +11,21 @@
 
 
 namespace fug {
+    struct SfKeyboardKeyHash {
+        template <typename T>
+        std::size_t operator()(T t) const
+        {
+            return static_cast<std::size_t>(t);
+        }
+    };
 
     class FUG_RESOURCE(SfmlInputConfig) {
     public:
         FUG_RESOURCE_INIT_DESTROY_DECL
         
         // <key, <player_id, action>>
-        using InputMap = std::unordered_map<sf::Keyboard::Key, std::pair<uint8_t, Input::Action>>;
+        using InputMap = std::unordered_map<sf::Keyboard::Key, std::pair<uint8_t, Input::Action>,
+                                            SfKeyboardKeyHash>;
 
         InputMap inputMap;
     };
