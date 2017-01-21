3d74128cbc03e24fbc77b44b7a88b71553d17a89
Veikka Kähkönen <insiding@gmail.com>
2017-01-21 07:49:16 +0200

    Make player movement more versatile

--- src/GameJam2017/PlayerInputVisitor.cpp
+++ src/GameJam2017/PlayerInputVisitor.cpp
@@ -1,5 +1,8 @@
 #include "Core/ResourceManager.hpp"
 #include "GameJam2017/PlayerInputVisitor.hpp"
+#include "GameJam2017/CharacterStateComponent.hpp"
+#include "GameJam2017/CharacterInfoComponent.hpp"
+#include "GameJam2017/PlayerComponent.hpp"
 #include "GameJam2017/constants.hpp"
 #include "Logic/SfmlInputConfig.hpp"
 
@@ -12,39 +15,37 @@ void PlayerInputVisitor::operator()(CharacterStateComponent& state,
     auto inputConfig = FUG_RESOURCE_MANAGER.getResource<SfmlInputConfig>(1);
     auto& inputMap = inputConfig->inputMap;
 
-    // Iteroi eventit läpi
     /*
+    // Iteroi eventit läpi
     if(inputMap.count(event.x.key.code) > 0) {
         auto playerAndAction = inputMap[event.x.key.code];
         if(!playerAndAction.first = player.playerId) {
             continue
         }
         bool keyPressed = (event.x.type == sf::Event::KeyPressed);
+        Vector3f dir(0.f, 0.f, 0.f);
         switch(playerAndAction.second) {
             Input::Up:
-                state.moveUp = keyPressed;
-                state.moveDown = false;
+                dir += keyPressed ? Vector3f(0, 1.f, 0) : 0;
                 break;
 
             Input::Down:
-                state.moveUp = false;
-                state.moveDown = keyPressed;
+                dir += keyPressed ? Vector3f(0, -1.f, 0) : 0;
                 break;
 
             Input::Left:
-                state.moveLeft = keyPressed;
-                state.moveRight = false;
+                dir += keyPressed ? Vector3f(-1.f, 0, 0) : 0;
                 break;
 
             Input::Right:
-                state.moveLeft = false;
-                state.moveRight = keyPressed;
+                dir += keyPressed ? Vector3f(1.f, 0, 0) : 0;
                 break;
 
             Input::Shoot:
                 // Ammu shootevent
                 break;
         }
+        state.movementDir = dir.normalized();
     }
     */
 }
