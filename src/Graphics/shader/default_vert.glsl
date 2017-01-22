bd028a1d2792a61bdf2c2fe86f2ec08aa45fff81
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-01-22 10:34:44 +0200

    Modify obj-parser to parse faces ccw

--- src/Graphics/shader/default_vert.glsl
+++ src/Graphics/shader/default_vert.glsl
@@ -2,8 +2,8 @@
 
 // Inputs
 layout(location = 0) in vec3 pos0;
-layout(location = 1) in vec3 norm0;
-layout(location = 2) in vec2 tex0;
+layout(location = 1) in vec2 tex0;
+layout(location = 2) in vec3 norm0;
 
 // Transformations
 uniform mat4 uModelToClip;
