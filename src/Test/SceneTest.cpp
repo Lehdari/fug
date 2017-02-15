03be1b6c9e65e0b38714ea61a40a4b8a208d7b60
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-02-15 02:19:12 +0200

    Tests: Split SceneTest from UnitTests

---
+++ src/Test/SceneTest.cpp
@@ -0,0 +1,46 @@
+#include "Test/UnitTests.hpp"
+
+#include "Core/Scene.hpp"
+
+#include "Test/TestComponents.hpp"
+#include "Test/TestVisitors.hpp"
+
+FUG_UNIT_TEST(sceneTest) {
+    using namespace fug;
+    EId nid[10] = {0};
+    uint64_t i = 0;
+
+    TestComponent1 tc11; tc11.a = 3; tc11.b = 4;
+    //TestComponent1 tc12; tc12.a = 5; tc12.b = 6;
+
+    //nid[i++] = FUG_SCENE.addEntity();
+    nid[i++] = FUG_SCENE.addEntity(TestComponent1(), TestComponent2(), TestComponent3());
+    nid[i++] = FUG_SCENE.addEntity();
+    nid[i++] = FUG_SCENE.addEntity(std::move(tc11), TestComponent2(), TestComponent3());
+    nid[i++] = FUG_SCENE.addEntity(TestComponent1(), TestComponent3());
+    nid[i++] = FUG_SCENE.addEntity(TestComponent1(), TestComponent2());
+
+    FUG_SCENE.print();
+
+    FUG_SCENE.removeEntity(nid[2]);
+
+    FUG_SCENE.print();
+
+    TestVisitor1 visitor1;
+    TestVisitor2 visitor2;
+    TestVisitor3 visitor3;
+
+    printf("Visiting with TestVisitor1:\n");
+    FUG_SCENE.accept(visitor1);
+    printf("Visiting with TestVisitor2:\n");
+    FUG_SCENE.accept(visitor2);
+    printf("Visiting with TestVisitor3:\n");
+    FUG_SCENE.accept(visitor3);
+
+
+    FUG_SCENE.removeEntity(nid[2]);
+
+    FUG_SCENE.print();
+}
+
+
--- src/Test/UnitTests.cpp
+++ src/Test/UnitTests.cpp
@@ -2,7 +2,6 @@
 
 #include <cstdio>
 
-#include "Core/Scene.hpp"
 #include "Test/TestVisitors.hpp"
 #include "Test/TestComponents.hpp"
 
@@ -82,44 +81,6 @@ void fug::Tester::fail(void)
 {
     if (_dieOnFail)
         throw;
-}
-
-FUG_UNIT_TEST(sceneTest) {
-    using namespace fug;
-    EId nid[10] = {0};
-    uint64_t i = 0;
-
-    TestComponent1 tc11; tc11.a = 3; tc11.b = 4;
-    //TestComponent1 tc12; tc12.a = 5; tc12.b = 6;
-
-    //nid[i++] = FUG_SCENE.addEntity();
-    nid[i++] = FUG_SCENE.addEntity(TestComponent1(), TestComponent2(), TestComponent3());
-    nid[i++] = FUG_SCENE.addEntity();
-    nid[i++] = FUG_SCENE.addEntity(std::move(tc11), TestComponent2(), TestComponent3());
-    nid[i++] = FUG_SCENE.addEntity(TestComponent1(), TestComponent3());
-    nid[i++] = FUG_SCENE.addEntity(TestComponent1(), TestComponent2());
-
-    FUG_SCENE.print();
-
-    FUG_SCENE.removeEntity(nid[2]);
-
-    FUG_SCENE.print();
-
-    TestVisitor1 visitor1;
-    TestVisitor2 visitor2;
-    TestVisitor3 visitor3;
-
-    printf("Visiting with TestVisitor1:\n");
-    FUG_SCENE.accept(visitor1);
-    printf("Visiting with TestVisitor2:\n");
-    FUG_SCENE.accept(visitor2);
-    printf("Visiting with TestVisitor3:\n");
-    FUG_SCENE.accept(visitor3);
-
-
-    FUG_SCENE.removeEntity(nid[2]);
-
-    FUG_SCENE.print();
 }
 
 FUG_UNIT_TEST(resourceTest) {
