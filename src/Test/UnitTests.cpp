6942c7932320d07f140ac4b1f7f22010871aa5be
Aapo Vienamo <aapo.vienamo@iki.fi>
2017-02-15 02:03:31 +0200

    Move drawTest to demos
    Renamed to textured_cube.

--- src/Test/UnitTests.cpp
+++ src/Test/UnitTests.cpp
@@ -17,36 +17,15 @@
 #include "Core/Utility.hpp"
 
 // Graphics includes
-#include "Graphics/Canvas.hpp"
 #include "Graphics/Canvas_SFML.hpp"
-
-#include "Graphics/Material.hpp"
-#include "Graphics/Material_Init.hpp"
-#include "Graphics/Mesh.hpp"
-#include "Graphics/Mesh_Init.hpp"
-#include "Graphics/MeshComponent.hpp"
-
-#include "Graphics/Renderer.hpp"
-#include "Graphics/SpriteRenderer.hpp"
 
 #include "Graphics/ShaderObject.hpp"
 #include "Graphics/ShaderObject_Init_Binary.hpp"
 #include "Graphics/ShaderProgram.hpp"
 #include "Graphics/ShaderProgram_Init_Default.hpp"
 
-#include "Graphics/SpriteMeshComponent.hpp"
-#include "Graphics/SpriteMaterial.hpp"
-#include "Graphics/SpriteMaterial_Init.hpp"
-
-#include "Graphics/SpriteMesh.hpp"
-#include "Graphics/SpriteMesh_Init.hpp"
-#include "Graphics/SpriteMeshComponent.hpp"
-
 #include "Graphics/Texture.hpp"
 #include "Graphics/Texture_Init_Binary.hpp"
-
-#include "Graphics/VertexData.hpp"
-#include "Graphics/VertexData_Init.hpp"
 
 fug::UnitTest::UnitTest(const std::string& testName,
                         fug::UnitTest::T_TestFunc testFunc) :
@@ -344,162 +323,3 @@ FUG_UNIT_TEST(eventTest) {
 
 }
 
-FUG_UNIT_TEST(drawTest)
-{
-    using namespace fug;
-    Canvas_SFML c;
-    sf::Window* wPtr = c.getWindow();
-
-    // Load resources
-
-    // SpriteTexture
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (100, BinaryInitInfo_File{"../res/textures/test_purjo.png"});
-    FUG_RESOURCE_MANAGER.addResourceInfo<Texture, TextureInitInfo_Binary>
-    (101, TextureInitInfo_Binary{TextureInitInfo_Binary::SOURCE_BINARY_PNG,
-        GL_CLAMP_TO_BORDER,
-        GL_CLAMP_TO_BORDER,
-        GL_NEAREST,
-        GL_NEAREST},
-     {100}, {}, true);
-
-    // Sprite ShaderProgram
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (102, BinaryInitInfo_File{"../src/Graphics/shader/sprite_vert.glsl"});
-    FUG_RESOURCE_MANAGER.addResourceInfo<ShaderObject, ShaderObjectInitInfo_Binary>
-    (103, ShaderObjectInitInfo_Binary{ShaderObjectInitInfo_Binary::SOURCE_GLSL,
-        GL_VERTEX_SHADER}, {102}, {});
-
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (104, BinaryInitInfo_File{"../src/Graphics/shader/sprite_frag.glsl"});
-    FUG_RESOURCE_MANAGER.addResourceInfo<ShaderObject, ShaderObjectInitInfo_Binary>
-    (105, ShaderObjectInitInfo_Binary{ShaderObjectInitInfo_Binary::SOURCE_GLSL,
-        GL_FRAGMENT_SHADER}, {104}, {});
-
-    FUG_RESOURCE_MANAGER.addResourceInfo<ShaderProgram, ShaderProgramInitInfo_Default>
-        (106, ShaderProgramInitInfo_Default{}, {103,105}, {});
-
-    // SpriteMaterial
-    FUG_RESOURCE_MANAGER.addResourceInfo<SpriteMaterial, SpriteMaterialInitInfo_Default>
-    (107, SpriteMaterialInitInfo_Default{{"diffuseSampler"},
-                                        {"uModelToClip"},
-                                        {"uSpriteW", "uSpriteH"},
-                                        {"uFrameRow", "uFrameColumn"},
-                                        32, 32},
-                                        {}, {106,101});
-
-    // SpriteMesh
-    FUG_RESOURCE_MANAGER.addResourceInfo<SpriteMesh, SpriteMeshInitInfo_Default>
-    (108, SpriteMeshInitInfo_Default(), {}, {107});
-
-    // SpriteMeshComponent
-    auto spriteMeshResPtr = FUG_RESOURCE_MANAGER.getResource<SpriteMesh>(108);
-    SpriteMeshComponent spriteMeshComp(spriteMeshResPtr);
-
-    // Default ShaderProgram
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (109, BinaryInitInfo_File{"../src/Graphics/shader/default_vert.glsl"});
-
-    FUG_RESOURCE_MANAGER.addResourceInfo<ShaderObject, ShaderObjectInitInfo_Binary>
-    (110, ShaderObjectInitInfo_Binary{ShaderObjectInitInfo_Binary::SOURCE_GLSL,
-        GL_VERTEX_SHADER}, {109}, {});
-
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (111, BinaryInitInfo_File{"../src/Graphics/shader/default_frag.glsl"});
-
-    FUG_RESOURCE_MANAGER.addResourceInfo<ShaderObject, ShaderObjectInitInfo_Binary>
-    (112, ShaderObjectInitInfo_Binary{ShaderObjectInitInfo_Binary::SOURCE_GLSL,
-        GL_FRAGMENT_SHADER}, {111}, {});
-
-    FUG_RESOURCE_MANAGER.addResourceInfo<ShaderProgram, ShaderProgramInitInfo_Default>
-    (113, ShaderProgramInitInfo_Default{}, {110,112}, {}, true);
-
-    // Cube Texture
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (114, BinaryInitInfo_File{"../res/textures/kuutio.png"});
-    FUG_RESOURCE_MANAGER.addResourceInfo<Texture, TextureInitInfo_Binary>
-    (115, TextureInitInfo_Binary{TextureInitInfo_Binary::SOURCE_BINARY_PNG,
-        GL_CLAMP_TO_BORDER,
-        GL_CLAMP_TO_BORDER,
-        GL_NEAREST,
-        GL_NEAREST},
-     {114}, {}, true);
-
-    // Cube Material
-    FUG_RESOURCE_MANAGER.addResourceInfo<Material, MaterialInitInfo_Default>
-    (116, MaterialInitInfo_Default{{"diffuseSampler"},
-                                   {"uModelToClip", "uModelToCam", "uNormalToCam"},
-                                   {"uSpecularCol"},
-                                   {"uSpecularExp"},
-                                   Vector3Glf(0.1f, 0.1f, 0.1f), 0.5f},
-                                   {}, {113,115}, true);
-
-    // Cube VertexData
-    FUG_RESOURCE_MANAGER.addResourceInfo<Binary, BinaryInitInfo_File>
-    (117, BinaryInitInfo_File{"../res/meshes/kuutio.obj"});
-    FUG_RESOURCE_MANAGER.addResourceInfo<VertexData, VertexDataInitInfo_Binary>
-    (118, VertexDataInitInfo_Binary{VertexDataInitInfo_Binary::SOURCE_BINARY_OBJ},
-        {117}, {}, true);
-
-    // Cube Mesh
-    FUG_RESOURCE_MANAGER.addResourceInfo<Mesh, MeshInitInfo_Default>
-    (119, MeshInitInfo_Default(), {118}, {116});
-
-    // Cube MeshComponent
-    auto cubeMeshResPtr = FUG_RESOURCE_MANAGER.getResource<Mesh>(119);
-    MeshComponent cubeMeshComp(cubeMeshResPtr);
-
-    Renderer renderer(Vector3Glf(0.f, 0.f, -3.f), Vector3Glf(0.f, 0.f, 1.f),
-                      Vector3Glf(0.f, 1.f, 0.f), 90.f, 1280/720.f, 1.f, 10.f);
-
-    SpriteRenderer spriteRenderer(Vector3Glf(0.f, 0.f, -3.f), Vector3Glf(0.f, 0.f, 1.f),
-                      Vector3Glf(0.f, 1.f, 0.f), 90.f, 1280/720.f, 1.f, 10.f);
-
-    bool running = true;
-    unsigned frame = 0;
-    unsigned anim = 0;
-    unsigned column = 0;
-    while (running)
-    {
-        // handle events
-        sf::Event event;
-        while (wPtr->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                // end the program
-                running = false;
-            }
-            else if (event.type == sf::Event::Resized)
-            {
-                // adjust the viewport when the window is resized
-                glViewport(0, 0, event.size.width, event.size.height);
-            }
-        }
-
-        // clear the buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        if (frame % 16 == 15) {
-            column = column < 7 ? column + 1 : 0;
-            if (column == 0) anim = anim < 7 ? anim + 1 : 0;
-        }
-        // draw...
-
-        TransformComponent transform;
-        transform.transform << 1.f, 0.f, 0.f, 1.f,
-                               0.f, 1.f, 0.f, -1.f,
-                               0.f, 0.f, 1.f, 0.f,
-                               0.f, 0.f, 0.f, 1.f;
-        spriteRenderer(spriteMeshComp, transform);
-        transform.transform << 1.f, 0.f, 0.f, -2.5f,
-                               0.f, 1.f, 0.f,  1.5f,
-                               0.f, 0.f, 1.f,  1.5f,
-                               0.f, 0.f, 0.f,   1.f;
-        renderer(cubeMeshComp, transform);
-
-        // end the current frame (internally swaps the front and back buffers)
-        wPtr->display();
-        frame++;
-    }
-}
