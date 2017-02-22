770a97c8fb53e177a81d07aec4552a468ed3c365
Keksi <juhanakekoni@yahoo.com>
2017-02-22 22:23:27 +0200

    Refactor canvas
    Canvas: Clean up hpp and move definitions to .tcc

--- include/Graphics/Canvas.hpp
+++ include/Graphics/Canvas.hpp
@@ -1,37 +1,20 @@
 #ifndef FUG_CORE_CANVAS_HPP
 #define FUG_CORE_CANVAS_HPP
 
-
-//#include <Core/EventBase.hpp>
-
 #include <memory>
 
 
 namespace fug {
 
-    template<typename CanvasType_T>
+    template <typename CanvasType_T>
     class Canvas {
+
     public:
-        Canvas(void) {}
-        ~Canvas(void) {}
-
-        bool isOpen(void) {
-            return static_cast<CanvasType_T*>(this)->isOpen();
-        }
-
-        void display(void) {
-            static_cast<CanvasType_T*>(this)->display();
-        }
-
-        void close(void) {
-            static_cast<CanvasType_T*>(this)->close();
-        }
-
-        void handleEvents(void) {
-            return static_cast<CanvasType_T*>(this)->handleEvents();
-        }
+        bool isOpen(void);
+        void display(void);
+        void close(void);
+        void handleEvents(void);
     };
 }
-
 
 #endif // FUG_CORE_CANVAS_HPP
