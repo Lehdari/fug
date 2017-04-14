55b33de0563da2cc3a8c10f83ac5f933bdbd07ad
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-04-14 19:16:12 +0300

    Graphics: Add basic GBuffer

---
+++ include/Graphics/GBuffer.hpp
@@ -0,0 +1,30 @@
+#ifndef FUG_GRAPHICS_GBUFFER_HPP
+#define FUG_GRAPHICS_GBUFFER_HPP
+
+#include <GL/glew.h>
+#include <vector>
+
+namespace fug {
+
+    class GBuffer {
+    public:
+        GBuffer(GLsizei resX, GLsizei resY, const std::vector<GLint>& sizedFormats,
+                const std::vector<GLint>& baseFormats);
+        ~GBuffer();
+
+        void resize(GLsizei resX, GLsizei resY);
+        void bindWrite();
+        void bindRead();
+
+    private:
+        GLuint              _fbo;
+        std::vector<GLuint> _textures;
+        GLuint              _depthTexture;
+        std::vector<GLint>  _sizedFormats;
+        std::vector<GLint>  _baseFormats;
+
+    };
+
+} // namespace fug
+
+#endif // FUG_GRAPHICS_GBUFFER_HPP
