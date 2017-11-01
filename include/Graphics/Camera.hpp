c702f58de86ac9226e7dda4a0eade7d34d35d3ef
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-11-01 21:42:04 +0200

    TODO: FIX CUBEDEMO? | Camera: Update api
    Add orient from position, forward vector and up
    Switch old member vectors with x,y,z axis and add getters

--- include/Graphics/Camera.hpp
+++ include/Graphics/Camera.hpp
@@ -16,19 +16,23 @@ namespace fug {
                  const float& near, const float& far);
 
         void lookAt(const Vector3Glf& from, const Vector3Glf& to, const Vector3Glf& up);
+        void orient(const Vector3Glf& pos, const Vector3Glf& fwd, const Vector3Glf& up);
         void projection(const float& fov, const float& aspectRatio,
                         const float& near, const float& far);
 
-        const Vector3Glf& getPosition(void) const;
+        const Vector3Glf& getXAxis(void) const;
+        const Vector3Glf& getYAxis(void) const;
+        const Vector3Glf& getZAxis(void) const;
+
         const Matrix4Glf getWorldToClip(void) const;
         const Matrix4Glf& getView(void) const;
         const Matrix4Glf& getProj(void) const;
 
     private:
-        //  Vectors for orientation matrix
-        Vector3Glf _position;
-        Vector3Glf _forward;
-        Vector3Glf _up;
+        // Orientation vectors
+        Vector3Glf _xAxis;
+        Vector3Glf _yAxis;
+        Vector3Glf _zAxis;
 
         //  Values for projection matrix
         float _fov;
--- src/Graphics/Camera.cpp
+++ src/Graphics/Camera.cpp
@@ -6,7 +6,6 @@ using namespace fug;
 Camera::Camera(const Vector3Glf& position, const Vector3Glf& forward, const Vector3Glf& up,
                const float& fov, const float& aspectRatio,
                const float& near, const float& far) :
-    _position(position), _forward(forward), _up(up),
     _fov(fov), _aspectRatio(aspectRatio), _near(near), _far(far)
 {
     projection(fov, aspectRatio, near, far);
@@ -16,22 +15,32 @@ Camera::Camera(const Vector3Glf& position, const Vector3Glf& forward, const Vect
 
 void Camera::lookAt(const Vector3Glf& from, const Vector3Glf& to, const Vector3Glf& up)
 {
-    Vector3Glf xAxis, yAxis, zAxis;
+    _zAxis = (to - from).normalized();
+    _xAxis = up.cross(_zAxis).normalized();
+    _yAxis = _zAxis.cross(_xAxis);
 
-    zAxis = (to - from).normalized();
-    xAxis = up.cross(zAxis).normalized();
-    yAxis = zAxis.cross(xAxis);
-
-    _orientation << xAxis[0], xAxis[1], xAxis[2], -xAxis.dot(from),
-                    yAxis[0], yAxis[1], yAxis[2], -yAxis.dot(from),
-                    zAxis[0], zAxis[1], zAxis[2], -zAxis.dot(from),
+    _orientation << _xAxis[0], _xAxis[1], _xAxis[2], -_xAxis.dot(from),
+                    _yAxis[0], _yAxis[1], _yAxis[2], -_yAxis.dot(from),
+                    _zAxis[0], _zAxis[1], _zAxis[2], -_zAxis.dot(from),
                          0.f,      0.f,      0.f,              1.f;
+}
+
+void Camera::orient(const Vector3Glf& pos, const Vector3Glf& fwd, const Vector3Glf& up)
+{
+    _zAxis = fwd.normalized();
+    _xAxis = up.cross(_zAxis).normalized();
+    _yAxis = _zAxis.cross(_xAxis);
+
+    _orientation << _xAxis[0], _xAxis[1], _xAxis[2], -_xAxis.dot(pos),
+                    _yAxis[0], _yAxis[1], _yAxis[2], -_yAxis.dot(pos),
+                    _zAxis[0], _zAxis[1], _zAxis[2], -_zAxis.dot(pos),
+                          0.f,       0.f,       0.f,              1.f;
 }
 
 void Camera::projection(const float& fov, const float& aspectRatio,
                         const float& near,const float& far)
 {
-    float r = tanf(fov / 2.0f);
+    float r = tanf(0.5f * fov);
     float t = r / aspectRatio;
     float farmnear = far - near;
 
@@ -41,9 +50,19 @@ void Camera::projection(const float& fov, const float& aspectRatio,
                        0.f,     0.f,                     1.f,                          0.f;
 }
 
-const Vector3Glf& Camera::getPosition(void) const
+const Vector3Glf& Camera::getXAxis(void) const
 {
-    return _position;
+    return _xAxis;
+}
+
+const Vector3Glf& Camera::getYAxis(void) const
+{
+    return _yAxis;
+}
+
+const Vector3Glf& Camera::getZAxis(void) const
+{
+    return _zAxis;
 }
 
 const Matrix4Glf Camera::getWorldToClip(void) const
