96e3db8a88b8ae56db24cd0104ea76c8b024b777
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-11-03 15:04:47 +0200

    TODO: FIX CUBEDEMO | Use RenderingMatrixType to bind rendering in runtime

--- include/Graphics/ShaderProgram_Init_Default.hpp
+++ include/Graphics/ShaderProgram_Init_Default.hpp
@@ -7,16 +7,19 @@
 #include "Graphics/ShaderProgram.hpp"
 #include "Graphics/ShaderObject.hpp"
 
+#include <tuple>
+
 namespace fug {
     struct ShaderProgramInitInfo_Default {
         std::vector<std::string> samplerNames;
         std::vector<std::string> uniformNames;
+        std::vector<std::pair<std::string, std::string>> matrixNames;
     };
     FUG_RESOURCE_INITINFO_INIT(ShaderProgram, ShaderProgramInitInfo_Default)
     {
         // Check that keys are valid
         assertJsonValidity("ShaderProgramInitInfo_Default", json,
-                          {"type", "samplerNames", "uniformNames"},
+                          {"type", "samplerNames", "uniformNames", "matrixNames"},
                           {});
 
         if (json.find("samplerNames") != json.end()) {
@@ -26,6 +29,10 @@ namespace fug {
         if (json.find("uniformNames") != json.end()) {
             for (auto& u : json["uniformNames"])
                 initInfo.uniformNames.push_back(u);
+        }
+        if (json.find("matrixNames") != json.end()) {
+            for (auto it = json["matrixNames"].begin(); it != json["matrixNames"].end(); it++)
+                initInfo.matrixNames.push_back({it.key(), it.value().get<std::string>()});
         }
     }
     FUG_RESOURCE_INIT(ShaderProgram, ShaderProgramInitInfo_Default)
@@ -58,6 +65,10 @@ namespace fug {
             _samplerLocations.emplace_back(glGetUniformLocation(programId_, name.c_str()));
         for (auto& name : initInfo.uniformNames)
             _uniformLocations.emplace_back(glGetUniformLocation(programId_, name.c_str()));
+        _matrixLocations = initRenderingMatrixLocations();
+        for (auto& matrix : initInfo.matrixNames)
+            _matrixLocations[static_cast<size_t>(getRenderingMatrixType(matrix.first))] =
+                glGetUniformLocation(programId_, matrix.second.c_str());
     }
     FUG_RESOURCE_DESTROY(ShaderProgram, ShaderProgramInitInfo_Default)
     {
