47f857320963e1b5fce09908165e5b5a388a04e8
Lehdari <miika.lehtimaki@gmail.com>
2018-04-03 01:16:24 +0300

    Add system macro and 2 additional test systems

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,33 @@
 #include "System.hpp"
 
 
-class TestSystem1 : public System<TestSystem1, int, float> {
+TECS_SYSTEM(TestSystem1, int, float) {
 public:
     void operator()(int& c1, float& c2) {
         ++c1;
         c2+=1.0f;
 
         printf("TestSystem1: %d, %0.2f\n", c1, c2);
+    }
+};
+
+TECS_SYSTEM(TestSystem2, int, char) {
+public:
+    void operator()(int& c1, char& c2) {
+        ++c1;
+        ++c2;
+
+        printf("TestSystem2: %d, %c\n", c1, c2);
+    }
+};
+
+TECS_SYSTEM(TestSystem3, long long unsigned, char) {
+public:
+    void operator()(long long unsigned& c1, char& c2) {
+        ++c1;
+        ++c2;
+
+        printf("TestSystem3: %llu, %c\n", c1, c2);
     }
 };
 
@@ -37,8 +57,21 @@ int main(void) {
     c.addComponent(3);
     c.addComponent(3llu);
 
+    c.addEntity();
+    c.addComponent('E');
+    c.addComponent(4.0f);
+    c.addComponent(4llu);
+
+
     TestSystem1 ts1;
     c.runSystem(ts1);
+
+    TestSystem2 ts2;
+    c.runSystem(ts2);
+
+    TestSystem3 ts3;
+    c.runSystem(ts3);
+
 
     return 0;
 }
