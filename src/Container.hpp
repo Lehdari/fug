4c080c583f2a6b372dd5731dcb1ccb685d4bce55
Lehdari <miika.lehtimaki@gmail.com>
2018-04-02 23:18:35 +0300

    Create component vector handling

--- src/Container.hpp
+++ src/Container.hpp
@@ -4,19 +4,29 @@
 
 #include <cstdint>
 #include <vector>
+#include <functional>
 
 
 class Container {
 public:
+    ~Container();
+
     template <typename T>
     void addComponent(const T& component);
 
 private:
-    std::vector<void*> _components;
+    std::vector<void*>                  _components;
+    std::vector<std::function<void()>>  _componentDeleters;
 
     static uint64_t typeIdCounter;
     template <typename T>
-    uint64_t typeId(void);
+    static uint64_t typeId(void);
+
+    template <typename T>
+    std::vector<T>& accessComponents(void);
+
+    template <typename T>
+    static void deleteComponents(void* components);
 };
 
 
@@ -24,6 +34,9 @@ template <typename T>
 void Container::addComponent(const T& component)
 {
     printf("typeId: %llu\n", typeId<T>());
+
+    auto& v = accessComponents<T>();
+    v.push_back(component);
 }
 
 template <typename T>
@@ -31,6 +44,25 @@ uint64_t Container::typeId(void)
 {
     static uint64_t tId = typeIdCounter++;
     return tId;
+}
+
+template<typename T>
+std::vector<T>& Container::accessComponents(void)
+{
+    auto tId = typeId<T>();
+    if (tId == _components.size()) {
+        _components.push_back(new std::vector<T>);
+        _componentDeleters.push_back(std::bind(&deleteComponents<T>, _components.back()));
+    }
+    return *static_cast<std::vector<T>*>(_components[tId]);
+}
+
+template<typename T>
+void Container::deleteComponents(void *components) {
+    auto* v = static_cast<std::vector<T>*>(components);
+    printf("deleted vector of type %llu, size %llu\n", typeId<T>(), v->size());
+
+    delete static_cast<std::vector<T>*>(components);
 }
 
 
