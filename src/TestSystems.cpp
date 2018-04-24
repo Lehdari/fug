68a9945222069407ee1cb7376a162ab12d74a101
Lehdari <miika.lehtimaki@gmail.com>
2018-04-25 01:44:05 +0300

    Remove debug prints

--- src/TestSystems.cpp
+++ src/TestSystems.cpp
@@ -22,23 +22,13 @@ void TestSystem2::operator()(TestComponent1& c1, TestComponent2& c2) {
 }
 
 void TestSystem3::operator()(TestComponent1& c1, TestComponent2& c2, TestComponent3& c3) {
-    //static uint64_t count = 0;
-    //printf("TestSystem3: %llu\n", ++count);
-    //if (count == 1000)
-    //    printf("benis1\n");
     ++c1.a;
-    //if (count == 1000)
-    //    printf("benis2\n");
     //++c1.b;
     //++c1.c;
     ++c2.a;
-    //if (count == 1000)
-    //    printf("benis3\n");
     //++c2.b;
     //++c2.c;
     ++c3.a;
-    //if (count == 1000)
-    //    printf("benis4\n");
     //++c3.b;
     //++c3.c;
 }
