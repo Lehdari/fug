78e36e8904ad990f7b353e2adc8e37858a8a897f
Lehdari <miika.lehtimaki@gmail.com>
2018-05-10 01:10:45 +0300

    Separate tests and library

---
+++ test/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "Tests.hpp"
+
+#include "Ecs.hpp"
+#include "TestSystems.hpp"
+
+
+int main(void) {
+
+    test2();
+/*
+    Ecs ecs;
+    ecs.addComponent(3, TestComponent1());
+    ecs.addComponent(1, TestComponent1());
+    ecs.addComponent(2, TestComponent1());
+    ecs.addComponent(0, TestComponent1());
+
+
+    TestSystem1 sys;
+    ecs.runSystem(sys);
+    printf("\n");
+    ecs.removeComponent<TestComponent1>(2);
+    ecs.runSystem(sys);
+    printf("\n");
+*/
+    return 0;
+}
