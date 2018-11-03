427206d9d9f73943764c3621dce55ad468376a46
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-11-04 01:19:05 +0200

    Add SpriteComponent

--- include/SpriteComponent.hpp
+++ include/SpriteComponent.hpp
@@ -4,11 +4,13 @@
 #include <SFML/Graphics/Sprite.hpp>
 
 struct SpriteComponent {
-    SpriteComponent(sf::Texture &texture, int colorIdx, int w, int h)
+    SpriteComponent(sf::Texture &texture, int idx, int w, int h) :
+        idx(idx)
     {
         sprite.setTexture(texture);
-        sprite.setTextureRect(sf::IntRect(colorIdx * w, 0, w, h));
+        sprite.setTextureRect(sf::IntRect(idx * w, 0, w, h));
     }
+    int idx;
     sf::Sprite sprite;
 };
 
