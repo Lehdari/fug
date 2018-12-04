dac2367aac7da0dd27381c73fda5c04eea022d44
Lehdari <miika.lehtimaki@gmail.com>
2018-12-04 19:23:40 +0200

    Move type definitions to separate file

---
+++ src/Types.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Lehdari on 4.12.2018.
+//
+
+#ifndef TEMPLATEECS_TYPES_HPP
+#define TEMPLATEECS_TYPES_HPP
+
+
+#include <cstdint>
+
+using EntityId = uint64_t;
+
+
+#endif //TEMPLATEECS_TYPES_HPP
