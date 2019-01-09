33e3f8e21f8142df4aeb88bf2a00228a44e3132a
Lehdari <miika.lehtimaki@gmail.com>
2019-01-09 12:05:49 +0200

    Add EventComponent

---
+++ include/EventComponent.hpp
@@ -0,0 +1,36 @@
+//
+// Created by Lehdari on 8.1.2019.
+//
+
+#ifndef ECSTESTGAME_EVENTCOMPONENT_HPP
+#define ECSTESTGAME_EVENTCOMPONENT_HPP
+
+
+#include "EventTypeId.hpp"
+#include "EventHandler.hpp"
+
+#include <unordered_map>
+#include <vector>
+
+
+class EventComponent {
+public:
+    friend class EventSystem;
+
+    template <typename T_Handler>
+    void addHandler();
+
+private:
+    std::unordered_map<int64_t, void(*)(void*)> _handlers;
+};
+
+
+template <typename T_Handler>
+void EventComponent::addHandler()
+{
+    auto etId = EventTypeId::typeId<typename T_Handler::EventType>();
+    _handlers[etId] = &EventHandler<T_Handler, typename T_Handler::EventType>::handleEvents;
+}
+
+
+#endif //ECSTESTGAME_EVENTCOMPONENT_HPP
