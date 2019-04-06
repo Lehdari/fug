e0b2f11341204edad5d9f1bfbd3dedc4bf215525
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2019-04-06 22:41:44 +0300

    Add LogicSystem

---
+++ include/LogicSystem.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Lehdari on 6.4.2019.
+//
+
+#ifndef ECSTESTGAME_LOGICSYSTEM_HPP
+#define ECSTESTGAME_LOGICSYSTEM_HPP
+
+
+#include <System.hpp>
+
+#include "LogicComponent.hpp"
+
+
+TECS_SYSTEM(LogicSystem, LogicComponent) {
+public:
+    void operator()(const EntityId& eId, LogicComponent& ec);
+};
+
+
+#endif //ECSTESTGAME_LOGICSYSTEM_HPP
---
+++ src/LogicSystem.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Lehdari on 6.4.2019.
+//
+
+#include "LogicSystem.hpp"
+
+
+void LogicSystem::operator()(const EntityId& eId, LogicComponent& ec)
+{
+
+}
