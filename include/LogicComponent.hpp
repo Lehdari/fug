812ef124c0e7bf93c6350e324887b56941befc0e
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2019-04-07 17:52:57 +0300

    LogicComponent: Add RO5 stuff

--- include/LogicComponent.hpp
+++ include/LogicComponent.hpp
@@ -6,8 +6,22 @@
 #define ECSTESTGAME_LOGICCOMPONENT_HPP
 
 
+#include <Types.hpp>
+
+
+class Ecs;
+
+
 struct LogicComponent {
+    friend class LogicSystem;
+
     LogicComponent();
+
+    LogicComponent(const LogicComponent& other);
+    LogicComponent(LogicComponent&& other);
+    LogicComponent operator=(const LogicComponent& other);
+    LogicComponent operator=(LogicComponent&& other);
+
     ~LogicComponent();
 
     template <typename T_Logic>
@@ -15,23 +29,48 @@ struct LogicComponent {
 
 private:
     void*   _logic;
+    void    (*_logicExecutor)(void*, Ecs& ecs, const EntityId& eId);
     void    (*_logicDeleter)(void*);
+    void    (*_logicCopier)(void*, LogicComponent&);
+
+    template <typename T_Logic>
+    static void executeLogic(void* logic, Ecs& ecs, const EntityId& eId);
 
     template <typename T_Logic>
     static void deleteLogic(void* logic);
+
+    template <typename T_Logic>
+    static void copyLogic(void* logic, LogicComponent& lc);
 };
 
 
-template<typename T_Logic>
+template <typename T_Logic>
 void LogicComponent::addLogic()
 {
     _logic = new T_Logic();
+    _logicExecutor = &executeLogic<T_Logic>;
+    _logicDeleter = &deleteLogic<T_Logic>;
+    _logicCopier = &copyLogic<T_Logic>;
 }
 
-template<typename T_Logic>
-void LogicComponent::deleteLogic(void *logic)
+
+template <typename T_Logic>
+void LogicComponent::executeLogic(void* logic, Ecs& ecs, const EntityId& eId)
 {
-    delete logic;
+    (*static_cast<T_Logic*>(logic))(ecs, eId);
+}
+
+template <typename T_Logic>
+void LogicComponent::deleteLogic(void* logic)
+{
+    delete static_cast<T_Logic*>(logic);
+}
+
+template <typename T_Logic>
+void LogicComponent::copyLogic(void* logic, LogicComponent& lc)
+{
+    lc.addLogic<T_Logic>();
+    *static_cast<T_Logic*>(lc._logic) = *static_cast<T_Logic*>(logic);
 }
 
 
--- src/LogicComponent.cpp
+++ src/LogicComponent.cpp
@@ -7,8 +7,54 @@
 
 LogicComponent::LogicComponent() :
     _logic          (nullptr),
-    _logicDeleter   (nullptr)
+    _logicExecutor  (nullptr),
+    _logicDeleter   (nullptr),
+    _logicCopier    (nullptr)
 {
+}
+
+LogicComponent::LogicComponent(const LogicComponent& other) :
+    LogicComponent()
+{
+    if (other._logicCopier)
+        other._logicCopier(other._logic, *this);
+}
+
+LogicComponent::LogicComponent(LogicComponent&& other) :
+    _logic          (other._logic),
+    _logicExecutor  (other._logicExecutor),
+    _logicDeleter   (other._logicDeleter),
+    _logicCopier    (other._logicCopier)
+{
+    other._logic         = nullptr;
+    other._logicExecutor = nullptr;
+    other._logicDeleter  = nullptr;
+    other._logicCopier   = nullptr;
+}
+
+LogicComponent LogicComponent::operator=(const LogicComponent& other)
+{
+    if (_logicDeleter)
+        _logicDeleter(_logic);
+
+    if (other._logicCopier)
+        other._logicCopier(other._logic, *this);
+}
+
+LogicComponent LogicComponent::operator=(LogicComponent&& other)
+{
+    if (_logicDeleter)
+        _logicDeleter(_logic);
+
+    _logic         = other._logic;
+    _logicExecutor = other._logicExecutor;
+    _logicDeleter  = other._logicDeleter;
+    _logicCopier   = other._logicCopier;
+
+    other._logic         = nullptr;
+    other._logicExecutor = nullptr;
+    other._logicDeleter  = nullptr;
+    other._logicCopier   = nullptr;
 }
 
 LogicComponent::~LogicComponent()
