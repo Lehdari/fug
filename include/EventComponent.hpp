5edc15459d297d6736705cac331235bbd511ed84
Lehdari <miika.lehtimaki@gmail.com>
2019-04-12 00:36:38 +0300

    EventComponent: Enable adding of LogicEventHandlers

--- include/EventComponent.hpp
+++ include/EventComponent.hpp
@@ -24,7 +24,11 @@ public:
     void addHandler();
 
 private:
-    std::unordered_map<int64_t, void(*)(Ecs& ecs, const EntityId&, void*)> _handlers;
+    using Handler = void(*)(Ecs& ecs, const EntityId& eId, void* events);
+    using LogicHandler = void(*)(Ecs& ecs, const EntityId& eId, void* logic, void* events);
+
+    std::unordered_map<int64_t, Handler>        _handlers;
+    std::unordered_map<int64_t, LogicHandler>   _logicHandlers;
 };
 
 
@@ -32,7 +36,13 @@ template <typename T_Handler>
 void EventComponent::addHandler()
 {
     auto etId = EventTypeId::typeId<typename T_Handler::EventType>();
-    _handlers[etId] = &EventHandler<T_Handler, typename T_Handler::EventType>::handleEvents;
+    if constexpr (T_Handler::hasLogic) {
+        _logicHandlers[etId] = &EventLogicHandler<T_Handler,
+            typename T_Handler::EventType, typename T_Handler::LogicType>::handleEvents;
+    }
+    else {
+        _handlers[etId] = &EventHandler<T_Handler, typename T_Handler::EventType>::handleEvents;
+    }
 }
 
 
