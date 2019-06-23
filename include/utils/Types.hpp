9f7876b4e4bc43a38771de1b9d550b643824b42f
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2019-06-23 23:43:11 +0300

    Utils: Add Types.hpp

---
+++ include/utils/Types.hpp
@@ -0,0 +1,25 @@
+//
+// Project: fug
+// File: Types.hpp
+//
+// Copyright (c) 2019 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENCE which is distributed
+// with this source code package.
+//
+
+#ifndef FUG_UTILS_TYPES_HPP
+#define FUG_UTILS_TYPES_HPP
+
+
+#include <cstdint>
+
+
+namespace fug {
+
+    using EntityId = int64_t;
+
+}
+
+
+#endif //FUG_UTILS_TYPES_HPP
