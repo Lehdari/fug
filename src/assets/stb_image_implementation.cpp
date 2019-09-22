5516407b2ffbc94fd398847d9574fbd3f6d9b0ea
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2019-09-22 23:16:52 +0300

    Add assets library

---
+++ src/assets/stb_image_implementation.cpp
@@ -0,0 +1,13 @@
+//
+// Project: fug
+// File: stb_image_implementation.cpp
+//
+// Copyright (c) 2019 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENCE which is distributed
+// with this source code package.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+#undef STB_IMAGE_IMPLEMENTATION
