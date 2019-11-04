4ca989ddb98e2796225488fb0629b1a5aa9fac53
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2019-11-04 15:04:10 +0200

    graphics: Add SpriteSheet

---
+++ include/graphics/SpriteSheet.hpp
@@ -0,0 +1,38 @@
+#ifndef FUG_GRAPHICS_SPRITESHEET_HPP
+#define FUG_GRAPHICS_SPRITESHEET_HPP
+
+
+#include <GL/glew.h>
+#include <ecs/Ecs.hpp>
+#include <glsprite.hh>
+#include <stb_image.h>
+#include <string>
+#include <cassert>
+
+
+namespace fug {
+
+    // TODO: This should be a resource
+    struct SpriteSheet {
+        SpriteSheet() = default;
+        SpriteSheet(const std::string &texturePath,
+                    int32_t textureW, int32_t textureH,
+                    uint32_t cellW, uint32_t cellH, float margin);
+        ~SpriteSheet();
+
+        SpriteSheet(const SpriteSheet&) = delete;
+        const SpriteSheet& operator=(const SpriteSheet&) = delete;
+
+        void load(const std::string &texturePath,
+                  int32_t textureW, int32_t textureH,
+                  uint32_t cellW, uint32_t cellH, float margin);
+
+        GLuint textureID = 0;
+        glsprite_sheet sheet = {};
+        glsprite_grid grid = {};
+    };
+
+} // namespace fug
+
+
+#endif // FUG_GRAPHICS_SPRITESHEET_HPP
