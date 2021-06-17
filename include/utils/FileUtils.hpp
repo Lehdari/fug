fd06b011888d876951d4f108e37d9d32c9c76213
Lehdari <miika.lehtimaki@gmail.com>
2021-06-18 00:43:47 +0300

    Utils: Add FileUtils.hpp

---
+++ include/utils/FileUtils.hpp
@@ -0,0 +1,18 @@
+//
+// Project: evolution_simulator_2
+// File: FileUtils.hpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef FUG_UTILS_FILEUTILS_HPP
+#define FUG_UTILS_FILEUTILS_HPP
+
+
+#define FUG_RES(PATH) (std::string(FUG_RES_DIR) + "/" + PATH)
+
+
+#endif //FUG_UTILS_FILEUTILS_HPP
